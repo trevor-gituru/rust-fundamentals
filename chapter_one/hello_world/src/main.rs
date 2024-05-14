8c1664f313ed6e49ac6cdb566d2952656ea59c74
Trevor Gituru <giturutrevor@gmail.com>
2024-05-14 10:53:11 +0000

    Hello World Rust project made with cargo

---
+++ chapter_one/hello_world/src/main.rs
@@ -0,0 +1,8 @@
+/*
+ * This program prints a string.
+ * The main functiin is entry point of the program.
+ * When '!' is used, then a macro is called & not a function.
+ */
+fn main() {
+    println!("Hello Rust World. My name is Trevor & I'm glad to be here.");
+}
