3db78b3491423e8ce87758977bccac1e7358084f
Trevor Gituru <giturutrevor@gmail.com>
2024-05-15 12:31:04 +0000

    Demonstrating numerical operations

---
+++ chapter_two/data_types/scalar/num_operation/src/main.rs
@@ -0,0 +1,16 @@
+/*
+ * The following program shows basic numerical operations that can be performed with rust
+ */
+fn main() {
+    //Addition
+    let sum = 5+8;
+    //Subtraction
+    let diff = 2.7-6.7;
+    //Multiplication
+    let mult = 12*5;
+    // Division
+    let div = -5/3;
+    //Remainder
+    let rem = 43%5;
+    println!("Sum: {}\nDifference : {}\nMultiplication : {}\nDivision : {}\nRemainder: {}\n", sum, diff, mult, div, rem);
+}
