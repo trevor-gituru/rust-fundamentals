55e4a0d7d212d81422aa195b567d6bfbb3ecfbe8
Trevor Gituru <giturutrevor@gmail.com>
2024-05-15 12:36:10 +0000

    Demonstrating boolean data types

---
+++ chapter_two/data_types/scalar/boolean/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * The following program demonstrates the boolean data type
+ */
+fn main() {
+    //Either true or false
+    let t = true;
+    //The keyword is as follows
+    let f: bool = false;
+    println!("The value of t: {}\nThe value of f: {}", t, f);
+}
