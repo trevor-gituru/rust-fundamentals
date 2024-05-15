cacb65d99ad3146e4aa649b27dd111da16024dac
Trevor Gituru <giturutrevor@gmail.com>
2024-05-15 14:35:53 +0000

    Added unit tuples

--- chapter_two/data_types/compound/tuples/src/main.rs
+++ chapter_two/data_types/compound/tuples/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let tup1: (i32, f64, u8) = (500, 6.8, 1);
     //To declare mutable tuple based on value
     let mut tup2 = (500, 6.8, 1);
+    // To declare an empty tuple known as unit
+    let tup3 =();
     //Destructuring a tuple with pattern matching
     let (x, y, z) = tup1;
     // Indexing a tuple 
@@ -16,7 +18,7 @@ fn main() {
     // Changing value of mutable tuple
     tup2.2 = 5;
     // Displaying values
-    println!("The first tuple: {:?}\nThe second tuple: {:?}", tup1, tup2);
+    println!("The first tuple: {:?}\nThe second tuple: {:?}\nThe unit tuple: {:?}\n", tup1, tup2, tup3);
     println!("Result of destructuring tup1:\nx: {:?}\ny: {:?}\nz: {:?}",x,y,z);
     println!("Result of indexing tup2:\nm1: {:?}\nm2: {:?}\nm3: {:?}",m1,m2,m3);
 
