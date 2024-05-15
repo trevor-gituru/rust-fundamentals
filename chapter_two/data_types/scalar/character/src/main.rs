92b5ba539525b4595dd8d6f7dd520129d7ece66c
Trevor Gituru <giturutrevor@gmail.com>
2024-05-15 12:49:01 +0000

    Demonstrating character types

---
+++ chapter_two/data_types/scalar/character/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * This program demonstrates character data type
+ */
+fn main() {
+    //Its character literal with single quotes
+    let c = 'z';
+    //Declaration wih explicit annotation
+    let z: char = 'Z';
+    println!("c contains: {}\nz contains: {}",c,z);
+}
